a9626cf1aaec573ad535e638d619a16c061edfe8
App <qwer4924809@gmail.com>
2021-09-06 17:12:30 +0800

    Project資料夾更新 貨幣轉換未完成

--- Project[02]/index1.js
+++ Project[02]/index1.js
@@ -1,6 +1,7 @@
+// https://www.youtube.com/watch?v=UY7F37KHyI8&t=1063s
 const dropList = document.querySelectorAll('.drop-list select');
 let fromCurrency = document.querySelector(".from select");
-let toCurrency = document.querySelectorAll(".to select");
+let toCurrency = document.querySelector(".to select");
 let getButton = document.querySelector("form button");
 for(let i = 0; i < dropList.length; i++){
     for(currency_code in country_code){
@@ -27,9 +28,11 @@ function getExchabgeRate(){
         amount.value = "1";
         amountVal = 1;
     }
-    let url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency.value}`;
+    let url = `https://v6.exchangerate-api.com/v6/dfc8d86280b1eae186693a90/latest/${fromCurrency.value}`;
     fetch(url).then(response => response.json()).then(result =>{
         let exchangeRate = result.conversion_rates[toCurrency.value];
-        console.log(exchangeRate);
+        let totalExchangeRate = (amountVal * exchangeRate).toFixed(2);
+        const exchangeRateTwxt = document.querySelector('.exchange-rate');
+        exchangeRateTwxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`;
     })
 }
